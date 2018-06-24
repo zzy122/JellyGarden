b83d483c2b071b7f2430296a9329cabde128d96e
my1325 <1173962595@qq.com>
2018-06-24 09:44:43 +0800

    报存

--- JellyGarden/VCs/UserInfo/VCs/UserInfoViewController.swift
+++ JellyGarden/VCs/UserInfo/VCs/UserInfoViewController.swift
@@ -10,29 +10,31 @@ import UIKit
 
 class UserInfoViewController: BaseMainViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+}
+
+extension UserInfoViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchSetting() {
+        
     }
-    */
-
+    
+    @IBAction func touchShare() {
+        
+    }
 }
---
+++ JellyGarden/VCs/UserInfo/Views/MineInfoHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  MineInfoHeaderView.swift
+//  JellyGarden
+//
+//  Created by weipinzhiyuan on 2018/6/24.
+//  Copyright © 2018年 zzy. All rights reserved.
+//
+
+import UIKit
+
+class MineInfoHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
