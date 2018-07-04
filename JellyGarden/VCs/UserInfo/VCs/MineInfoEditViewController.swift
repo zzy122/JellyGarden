bfde06e32d34f2fa97a4e25a2cca1003b65961d6
my1325 <1173962595@qq.com>
2018-07-04 17:46:47 +0800

    保存

--- JellyGarden/VCs/UserInfo/VCs/MineInfoEditViewController.swift
+++ JellyGarden/VCs/UserInfo/VCs/MineInfoEditViewController.swift
@@ -16,9 +16,8 @@ class MineInfoEditViewController: BaseMainViewController {
             tableView.separatorColor = hexString(hex: "f3f5f8")
             tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
             
-            let tags1 = TagsView(.text) //OK
             tags1.backgroundColor = UIColor.clear
-            tags1.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: 200)
+            tags1.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: 250)
             tags1.padding = 10
             tags1.isEnableAdd = false
             tags1.tagDelegate = self
@@ -30,10 +29,15 @@ class MineInfoEditViewController: BaseMainViewController {
                                                   textColor: hexString(hex: "666666"),
                                                   font: UIFont.systemFont(ofSize: 14))
 
-            tags1.titles = ["女汉子", "女神", "女神经", "苗条", "学霸", "购物狂", "绿茶", "贤惠", "酒神", "厨艺好", "文艺", "好身材", "欧美范儿", "纹身", "AJ控", "运动", "马甲线", "电影", "看书", "旅游", "吃货", "走肾不开心"]
+            tags1.titles = FillCondition.share.conditionTag
             tableView.tableFooterView = tags1
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.35) {
+                self.reloadTag()
+            }
         }
     }
+    
+    let tags1 = TagsView(.text) //OK
     
     let textView: UITextView = {
         let _textView = UITextView()
@@ -46,10 +50,15 @@ class MineInfoEditViewController: BaseMainViewController {
         return _textView
     }()
     
+    var userInfo: UserModel! = CurrentUserInfo
+    
+    var newUserInfo: UserModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "编辑资料"
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -73,6 +82,17 @@ extension MineInfoEditViewController: TagsViewDelegate {
     func tagsView(didTouchTagAtIndex index: Int) {
         
     }
+    
+    func reloadTag() {
+        for selfTag in userInfo.data?.tags ?? [] {
+            for index in 0 ..< FillCondition.share.conditionTag.count {
+                let tag = FillCondition.share.conditionTag[index]
+                if tag == selfTag {
+                    tags1.selectItem(at: IndexPath(item: index, section: 0), animated: false, scrollPosition: UICollectionViewScrollPosition.top)
+                }
+            }
+        }
+    }
 }
 
 extension MineInfoEditViewController: UITableViewDelegate {
@@ -89,6 +109,66 @@ extension MineInfoEditViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        // 头像
+        if 0 == indexPath.section && 0 == indexPath.row {
+            UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        }
+        // 昵称
+        else if 0 == indexPath.section && 1 == indexPath.row {
+            
+        }
+        // 约会范围
+        else if 0 == indexPath.section && 2 == indexPath.row {
+            
+        }
+            // 年龄
+        else if 0 == indexPath.section && 3 == indexPath.row {
+            
+        }
+            /// 身高
+        else if 0 == indexPath.section && 4 == indexPath.row {
+            
+        }
+            // 身份
+        else if 0 == indexPath.section && 5 == indexPath.row {
+            
+        }
+            // 体重
+        else if 0 == indexPath.section && 6 == indexPath.row {
+            
+        }
+            // 胸围
+        else if 0 == indexPath.section && 7 == indexPath.row {
+            
+        }
+            // 打扮风格
+        else if 1 == indexPath.section && 0 == indexPath.row {
+            
+        }
+            /// 语言
+        else if 1 == indexPath.section && 1 == indexPath.row {
+            
+        }
+            // 感情
+        else if 1 == indexPath.section && 2 == indexPath.row {
+            
+        }
+            // 约会节目
+        else if 1 == indexPath.section && 3 == indexPath.row {
+            
+        }
+            // 约会条件
+        else if 1 == indexPath.section && 4 == indexPath.row {
+            
+        }
+            // QQ
+        else if 2 == indexPath.section && 0 == indexPath.row {
+            
+        }
+            // wechat
+        else if 2 == indexPath.section && 1 == indexPath.row {
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
@@ -181,6 +261,7 @@ extension MineInfoEditViewController: UITableViewDataSource {
             cell?.textLabel?.text = "头像"
             cell?.detailTextLabel?.text = ""
             let imageView = UIImageView()
+            imageView.kf.setImage(with: URL(string: userInfo.data?.avatar ?? ""))
             imageView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
             imageView.layer.cornerRadius = 20
             imageView.clipsToBounds = true
@@ -188,65 +269,66 @@ extension MineInfoEditViewController: UITableViewDataSource {
         }
         else if 1 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "昵称"
-            cell?.detailTextLabel?.text = "KK"
+            cell?.detailTextLabel?.text = userInfo.data?.nickname
         }
         else if 2 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "约会范围"
-            cell?.detailTextLabel?.text = "北京市"
+            cell?.detailTextLabel?.text = userInfo.data?.appointment_place?.joined(separator: " ")
         }
         else if 3 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "年龄"
-            cell?.detailTextLabel?.text = "18岁"
+            cell?.detailTextLabel?.text = "\(userInfo.data?.age ?? 18)岁"
         }
         else if 4 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "身份"
-            cell?.detailTextLabel?.text = "模特"
+            cell?.detailTextLabel?.text = userInfo.data?.identity
         }
         else if 5 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "身高"
-            cell?.detailTextLabel?.text = "173CM"
+            cell?.detailTextLabel?.text = "\(userInfo.data?.stature ?? 165)CM"
         }
         else if 6 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "体重"
-            cell?.detailTextLabel?.text = "46KG"
+            cell?.detailTextLabel?.text = "\(userInfo.data?.weight ?? 40)KG"
         }
         else if 7 == indexPath.row && 0 == indexPath.section {
             cell?.textLabel?.text = "胸围"
-            cell?.detailTextLabel?.text = "36D"
+            cell?.detailTextLabel?.text = "\(userInfo.data?.bust ?? 36)CM"
         }
         else if 0 == indexPath.row && 1 == indexPath.section {
             cell?.textLabel?.text = "打扮风格"
-            cell?.detailTextLabel?.text = "连衣裙"
+            cell?.detailTextLabel?.text = userInfo.data?.dress_style?.joined(separator: " ")
         }
         else if 1 == indexPath.row && 1 == indexPath.section {
             cell?.textLabel?.text = "语言"
-            cell?.detailTextLabel?.text = "普通话"
+            cell?.detailTextLabel?.text = userInfo.data?.language?.joined(separator: " ")
         }
         else if 2 == indexPath.row && 1 == indexPath.section {
             cell?.textLabel?.text = "感情"
-            cell?.detailTextLabel?.text = "单身"
+            cell?.detailTextLabel?.text = userInfo.data?.emotion_status
         }
         else if 3 == indexPath.row && 1 == indexPath.section {
             cell?.textLabel?.text = "约会节目"
-            cell?.detailTextLabel?.text = "旅游 看电影"
+            cell?.detailTextLabel?.text = userInfo.data?.appointment_program?.joined(separator: " ")
         }
         else if 4 == indexPath.row && 1 == indexPath.section {
             cell?.textLabel?.text = "约会条件"
-            cell?.detailTextLabel?.text = "看情况"
+            cell?.detailTextLabel?.text = userInfo.data?.appointment_condition?.joined(separator: " ")
         }
         else if 0 == indexPath.row && 2 == indexPath.section {
             cell?.textLabel?.text = "QQ"
-            cell?.detailTextLabel?.text = "1173962595"
+            cell?.detailTextLabel?.text = userInfo.data?.contact_qq
         }
         else if 1 == indexPath.row && 2 == indexPath.section {
             cell?.textLabel?.text = "微信"
-            cell?.detailTextLabel?.text = "就是垃圾发"
+            cell?.detailTextLabel?.text = userInfo.data?.contact_wechat
         }
         else if 3 == indexPath.section {
             var cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell3")
             if cell == nil {
                 cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "UITableViewCell3")
                 cell?.contentView.addSubview(textView)
+                textView.text = userInfo.data?.self_introduction
                 textView.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: 120)
             }
             return cell!
